f903a32f5261ce379560a3717e48afdaac3639e2
Kohsuke Kawaguchi <kk@kohsuke.org>
2011-09-27 17:55:49 -0700

    revision not yet available

--- src/main/resources/jenkins/plugins/svnmerge/IntegrateAction/inProgress.groovy
+++ src/main/resources/jenkins/plugins/svnmerge/IntegrateAction/inProgress.groovy
@@ -8,10 +8,8 @@ l.layout(norefresh:"true", title:"#${my.build.number} Integration") {
     l.main_panel {
         h1 {
             img(src:"${rootURL}/plugin/svnmerge/48x48/integrate.gif")
-            text(_("title", my.integratedRevision))
+            text(_("title"))
         }
-
-        p(_("Integration is in progress."))
 
         include(my,"log")
     }
