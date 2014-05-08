25c0f336565e2d7a93155991c6fc28de408d1dc8
vernyquartara <webny23@gmail.com>
2014-05-08 14:59:37 +0200

    The defaults values are now displayed just above the input fields. Added some properties.

--- src/main/resources/jenkins/plugins/svnmerge/IntegratableProjectAction/index.groovy
+++ src/main/resources/jenkins/plugins/svnmerge/IntegratableProjectAction/index.groovy
@@ -27,12 +27,6 @@ l.layout(norefresh:"true",title:_("title",my.project.displayName)) {
 		if (StringUtils.isNotEmpty(repoLayout.subProjectName)) {
 			p(_("Detected subproject name: "+repoLayout.subProjectName))
 		}
-		if (repoLayout.layout!=CUSTOM) {
-			p(_("Default new branch location: "+repoLayout.defaultNewBranchUrl))
-			p(_("Default new tag location: "+repoLayout.defaultNewDevTagUrl))
-		} else {
-			p(_("Unable to detect default branch or tag locations for custom layout repositories"))
-		}
 
         def branches = my.branches;
         if (branches.size()>0) {
@@ -51,58 +45,84 @@ l.layout(norefresh:"true",title:_("title",my.project.displayName)) {
         p {
             form (name:"new", method:"post", action:"newBranch") {
 				
-				raw("<table>")
-				
-				raw("<tr>")
-				raw("<td>")
-				text(_("Branch Name")+":")
-				raw("</td>")
-				raw("<td>")
-				input (type:"text", name:"name", width:"30")
-				raw("</td>")
-				raw("</tr>")
-				
-				raw("<tr>")
-				raw("<td>")
-				text(_("Commit Message")+":")
-				raw("</td>")
-				raw("<td>")
-				input (type:"text", name:"commitMessage")
-				raw("</td>")
-				raw("</tr>")
-				
-				raw("<tr>")
-				raw("<td>")
-				text(_("Override default branch location")+":")
-				raw("</td>")
-				raw("<td>")
-				input (type:"text", name:"branchLocation", width:"90")
-				raw("</td>")
-				raw("</tr>")
-				
-				raw("<tr>")
-				raw("<td>")
-				text(_("Create a development tag")+":")
-				raw("</td>")
-				raw("<td>")
-				input (type:"checkbox", name:"createTag", width:"30")
-				raw("</td>")
-				raw("</tr>")
-				
-				raw("<tr>")
-				raw("<td>")
-				text(_("Override default tag location")+":")
-				raw("</td>")
-				raw("<td>")
-				input (type:"text", name:"tagLocation")
-				raw("</td>")
-				raw("</tr>")
-				
-				raw("</table>")
-				
-				input (type:"hidden", name:"defaultNewBranchUrl", value: repoLayout.defaultNewBranchUrl)
-				input (type:"hidden", name:"defaultNewDevTagUrl", value: repoLayout.defaultNewDevTagUrl)
-				input (type:"hidden", name:"layout", 			  value: repoLayout.layout.name)
+                table (width: "100%") {
+
+                    tr {
+                        td (width: "25%") {
+                            text(_("Branch Name")+":")
+                        }
+                        td {
+                            input (type:"text", name:"name", size:"90")
+                        }
+                    }
+                
+                    tr {
+                        td {
+                            text(_("Commit Message")+":")
+                        }
+                        td {
+                            input (type:"text", name:"commitMessage", size:"90")
+                        }
+                    }
+					
+					tr {
+						td {
+							text(_("Default new branch location")+":")
+						}
+						td {
+							def value = repoLayout.defaultNewBranchUrl
+							if (repoLayout.layout==CUSTOM) {
+								value = _("unableToDetect","branches")
+							}
+							input (type:"text", name:"defaultNewBranchUrl", size:"90",
+								   disabled: true, value: value)
+						}
+					}
+                
+                    tr {
+                        td {
+                             text(_("override", "branch")+":")
+                        }
+                        td {
+                            input (type:"text", name:"branchLocation", size:"90")
+                        }
+                    }
+                
+                    tr {
+                        td {
+                            text(_("Create a development tag")+":")
+                        }
+                        td {
+                            input (type:"checkbox", name:"createTag", width:"90")
+                        }
+                    }
+					
+					tr {
+						td {
+							text(_("Default new tag location")+":")
+						}
+						td {
+							def value = repoLayout.defaultNewDevTagUrl
+							if (repoLayout.layout==CUSTOM) {
+								value = _("unableToDetect","tags")
+							}
+							input (type:"text", name:"defaultNewDevTagUrl", size:"90",
+								   disabled: true, value: value)
+						}
+					}
+                
+                    tr {
+                        td {
+                            text(_("override", "tag")+":")
+                        }
+                        td {
+                            input (type:"text", name:"tagLocation", size:"90")
+                        }
+                    }
+                
+                }
+                
+                input (type:"hidden", name:"layout", 			  value: repoLayout.layout.name)
 				
                 f.submit(value:_("Create"))
             }
